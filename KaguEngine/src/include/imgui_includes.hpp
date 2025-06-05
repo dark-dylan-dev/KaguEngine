8ca25ac9112da626a6a33c94bce1ed12d29f8de6
dark-dylan-dev <d40598408@gmail.com>
2025-06-05 22:54:18 +0200

    Simplified include directives.

--- KaguEngine/src/include/imgui_includes.hpp
+++ KaguEngine/src/include/imgui_includes.hpp
@@ -1,14 +1,9 @@
 #pragma once
 
 #include <imgui.cpp>
-#include <imgui.h>
-#include <imgui_internal.h>
-#include <imconfig.h>
 #include <imgui_demo.cpp>
 #include <imgui_draw.cpp>
 #include <imgui_tables.cpp>
 #include <imgui_widgets.cpp>
-#include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_glfw.cpp>
-#include <backends/imgui_impl_vulkan.h>
 #include <backends/imgui_impl_vulkan.cpp>
